609cbd95782e7314c2aea9d08c99422d49c2c36c
kdm565 <1476045903@qq.com>
2018-07-09 15:09:46 +0800

    support camera phone

--- static/js/init.js
+++ static/js/init.js
@@ -40,9 +40,23 @@ if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
 
     navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
         video.src = window.URL.createObjectURL(stream);
-        appear_video.src = window.URL.createObjectURL(stream);
-        //video.play();
+        video.play();
     });
+}else if(navigator.getUserMedia) { // Standard
+    navigator.getUserMedia({ video: true }, function(stream) {
+        video.src = stream;
+        video.play();
+    }, errBack);
+} else if(navigator.webkitGetUserMedia) { // WebKit-prefixed
+    navigator.webkitGetUserMedia({ video: true }, function(stream){
+        video.src = window.webkitURL.createObjectURL(stream);
+        video.play();
+    }, errBack);
+} else if(navigator.mozGetUserMedia) { // Mozilla-prefixed
+    navigator.mozGetUserMedia({ video: true }, function(stream){
+        video.src = window.URL.createObjectURL(stream);
+        video.play();
+    }, errBack);
 }
 document.onkeyup = function (e) {//按键信息对象以函数参数的形式传递进来了，就是那个e
     var code = e.charCode || e.keyCode;  //取出按键信息中的按键代码(大部分浏览器通过keyCode属性获取按键代码，但少部分浏览器使用的却是charCode)
